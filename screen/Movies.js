deb94c2e186982cef64bc822d149f16308097b65
YS739 <suk921@gmail.com>
2023-01-03 12:38:29 +0900

    Add useFocusEffect compare with useEffect

--- screen/Movies.js
+++ screen/Movies.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import React, { useCallback, useEffect } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Movies({ navigation: { navigate } }) {
+  useEffect(() => {
+    console.log("마운트");
+    return () => {
+      console.log("언마운트");
+    };
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log("Focus");
+      return () => {
+        console.log("Blur");
+      };
+    }, [])
+  );
+
   return (
     <View>
       <Text> Movies </Text>
