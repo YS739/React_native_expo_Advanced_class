447c7736bca1d5bc0a4e94e4c2e0b229b04c0859
YS739 <suk921@gmail.com>
2023-01-03 10:48:25 +0900

    Add reset, setOptions props

--- App.js
+++ App.js
@@ -13,19 +13,42 @@ const One = ({ navigation: { navigate } }) => {
   );
 };
 
-const Two = ({ navigation: { navigate } }) => {
+const Two = ({ navigation: { navigate, setOptions } }) => {
   return (
-    <TouchableOpacity onPress={() => navigate("three")}>
-      <Text>Two</Text>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity onPress={() => navigate("three")}>
+        <Text>Two</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          setOptions({
+            title: "제목 바꾸기",
+          })
+        }
+      >
+        <Text>Set Options</Text>
+      </TouchableOpacity>
+    </>
   );
 };
 
-const Three = ({ navigation: { goBack } }) => {
+const Three = ({ navigation: { goBack, reset } }) => {
   return (
-    <TouchableOpacity onPress={() => goBack()}>
-      <Text>Three</Text>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity onPress={() => goBack()}>
+        <Text>Three</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          reset({
+            index: 2,
+            routes: [{ name: "three" }, { name: "one" }, { name: "two" }],
+          })
+        }
+      >
+        <Text>Reset Navigation</Text>
+      </TouchableOpacity>
+    </>
   );
 };
 
