a82faa6c7d00410c503a76127069c62c6a0f7225
YS739 <suk921@gmail.com>
2023-01-03 10:29:34 +0900

    Add navigation props

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+const One = ({ navigation: { navigate } }) => {
+  return (
+    <TouchableOpacity onPress={() => navigate("two")}>
+      <Text>One</Text>
+    </TouchableOpacity>
+  );
+};
+
+const Two = ({ navigation: { navigate } }) => {
+  return (
+    <TouchableOpacity onPress={() => navigate("three")}>
+      <Text>Two</Text>
+    </TouchableOpacity>
+  );
+};
+
+const Three = ({ navigation: { goBack } }) => {
+  return (
+    <TouchableOpacity onPress={() => goBack()}>
+      <Text>Three</Text>
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="one" component={One} />
+        <Stack.Screen name="two" component={Two} />
+        <Stack.Screen name="three" component={Three} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
