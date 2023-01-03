2df5d93ff3c9467be5c2b0998c2fd1ef6cc3d703
YS739 <suk921@gmail.com>
2023-01-03 11:28:03 +0900

    Add bottom tab props

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,14 +1,47 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import Movies from "../screen/Movies";
 import My from "../screen/My";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Movies" component={Movies} />
-      <Tab.Screen name="My" component={My} />
+    <Tab.Navigator
+      initialRouteName="my"
+      sceneContainerStyle={{
+        backgroundColor: "lavender",
+      }}
+      screenOptions={{
+        tabBarLabelPosition: "beside-icon",
+      }}
+    >
+      <Tab.Screen
+        options={{
+          title: "영화",
+          headerTitleAlign: "center",
+          tabBarLabel: "Movies",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="movie" size={size} color={color} />
+          ),
+        }}
+        name="Movies"
+        component={Movies}
+      />
+      <Tab.Screen
+        options={{
+          title: "내가 작성한 댓글",
+          tabBarLabel: "My",
+          tabBarBadge: "hi",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="user" size={size} color={color} />
+          ),
+        }}
+        name="My"
+        component={My}
+      />
     </Tab.Navigator>
   );
 };
